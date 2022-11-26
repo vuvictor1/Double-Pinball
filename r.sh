0b6c75d37a91082a3280f8e56d2049aba6401b21
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2022-11-26 11:49:02 -0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
-#;*******************************************************************************************
-#; Author: Victor V. Vu
-#: Section: 223N-01
-#; Descrption: BASH compilation file
-#;
-#; Copyright (C) 2022 Victor V. Vu
-#; This program is free software: you can redistribute it and/or modify it under the terms
-#; of the GNU General Public License version 3 as published by the Free Software Foundation.
-#; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY
-#; without even the implied Warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#; See the GNU General Public License for more details. A copy of the GNU General Public
-#; License v3 is available here:  <https://www.gnu.org/licenses/>.
-#;*******************************************************************************************
-#; Programmed in Ubuntu-based Linux Platform.
-#; To run program, type in terminal: "sh r.sh"
-#********************************************************************************************
+# Author: Victor V. Vu
+# Descrption: BASH compilation file
 
-echo Removing old binary files
+# Copyright (C) 2022 Victor V. Vu
+# This program is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License version 3 as published by the Free Software Foundation.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY
+# without even the implied Warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details. A copy of the GNU General Public
+# License v3 is available here:  <https://www.gnu.org/licenses/>.
+
+# Programmed in Ubuntu-based Linux Platform.
+# To run bash script, type in terminal: "sh r.sh"
+
+# Remove old dll and executables
 rm *.dll
 rm *.exe
 
-echo Viewing list of source files
+# View list of source files
 ls -l
 
-echo Compiling C-Sharp file to create new dlls
+# Compiling C# file to create new dlls
 mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -out:DoublePinballui.dll DoublePinballui.cs
 
-echo Compiling the driver file and linking them with the previously created dll files, creating a new executable.
+# Compiling the driver file and linking with created dlls to create an executable
 mcs -r:System -r:System.Windows.Forms -r:DoublePinballui.dll -out:DoublePinball.exe DoublePinballmain.cs
 
-echo Viewing current list of source files
+# View current list of source files
 ls -l
 
-echo Running the executable file.
+# Launch the program
 ./DoublePinball.exe
-
-echo The script has terminated.
